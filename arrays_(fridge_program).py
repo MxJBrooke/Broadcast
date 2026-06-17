a5cf637cd10394b6b3928465aa2582fefd36b3ed
Mx Brooke <brooketj2@gmail.com>
2026-06-17 10:38:25 +0100

    Add files via upload

---
+++ arrays_(fridge_program).py
@@ -0,0 +1,19 @@
+
+fridge = ["carrots", "fish", "milk", "eggs"]
+
+print(fridge)
+
+fridge.append("eggs")
+print(fridge)
+
+fridge.pop(3)
+print(fridge)
+
+start = 0
+end = 5
+for counter in range(start, end):
+    print(counter)
+
+
+for i in range(20,0,-1):
+    print(i)
